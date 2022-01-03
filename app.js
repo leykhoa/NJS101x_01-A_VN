e1e05e9879c85f6daa86c8d4dfb8ccfc360f7ee5
KhoaLe <leykhoaqk@gmail.com>
2022-01-03 23:51:29 +0700

    LAB 2.4

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
+app.use('/', (req, res, next) => {
+    next();
+});
+
+app.use('/add-product', (req, res, next) => {
+    console.log('In the another middleware');
+    res.send('<h1>The "Add product" Page</h1>');
     next();
 });
 
 app.use((req, res, next) => {
     console.log('In the another middleware');
-    res.send('<h1>Hello from Express</h1>');
+    res.send('<h1>Hello from Express.js</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000)
