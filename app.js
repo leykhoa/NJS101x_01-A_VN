98e0f30a9db754b6ec3693c40802d51b1a98c959
KhoaLe <leykhoaqk@gmail.com>
2022-01-04 13:56:49 +0700

    LAB 2.10-2.14

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const http = require('http');
 
 const express = require('express');
@@ -10,13 +11,14 @@ const shopRoutes = require('./routes/shop');
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 
 app.use(shopRoutes);
 
 app.use((req, res) => {
-    res.status(404).send('<h1>Page not found!</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);
