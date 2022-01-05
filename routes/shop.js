12049248b6056b0a0935ffefeda56c45ffa415df
KhoaLe <leykhoaqk@gmail.com>
2022-01-05 16:06:35 +0700

    LAB3.5-3.6

--- routes/shop.js
+++ routes/shop.js
@@ -10,8 +10,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     const products = adminData.products;
-    res.render('shop', { prods: products, docTitle: 'Shop' })
-    console.log('check prods', products)
+    res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' })
 });
 
 module.exports = router
