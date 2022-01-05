5189a340dd6e001aa7a995ddccba82dcafedb32e
KhoaLe <leykhoaqk@gmail.com>
2022-01-05 23:31:53 +0700

    LAB 4.1 & 4.2

---
+++ controller/products.js
@@ -0,0 +1,32 @@
+const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        activeAddProduct: true,
+        productCSS: true,
+        formsCSS: true
+    });
+
+};
+
+exports.postAddProduct = (req, res, next) => {
+    products.push({ title: req.body.title })
+    res.redirect('/')
+};
+
+exports.getProducts = (req, res, next) => {
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    })
+};
+
+exports.Error404 = (req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+};
