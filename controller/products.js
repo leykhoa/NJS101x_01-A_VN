77242be73c66bdf7c52098ca5dc91b624743f5c4
KhoaLe <leykhoaqk@gmail.com>
2022-01-08 14:52:13 +0700

    LAB 4.3 - 4.6

--- controller/products.js
+++ controller/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
@@ -12,21 +12,22 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title })
+    const product = new Product(req.body.title)
+    product.save()
     res.redirect('/')
 };
 
 exports.getProducts = (req, res, next) => {
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-    })
+    Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        })
+    });
+
 };
 
-exports.Error404 = (req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
-};
