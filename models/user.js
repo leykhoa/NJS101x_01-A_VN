a99a751517bb133e4e5ebf527b09850a52ac8082
KhoaLe <leykhoaqk@gmail.com>
2022-02-18 21:35:55 +0700

    LAB 6.20

--- models/user.js
+++ models/user.js
@@ -61,6 +61,18 @@ class User {
         }
         )))
   }
+
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter(
+      item => item.productId.toString() !== productId.toString())
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } }
+      )
+  }
   static findByPk(userId) {
     const db = getDb();
     return db
