2dd28e0a81be8d9229220a9356afd866a06b9efb
KhoaLe <leykhoaqk@gmail.com>
2022-02-22 08:42:07 +0700

    LAB 7.8

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user')
+const User = require('./models/user')
 
 
 
@@ -20,14 +20,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findByPk('620f1ebd0c7220350a211de7')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findByPk('6214385e0aac643de14c4406')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,7 +36,19 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://khoale:0712@cluster0.xszjz.mongodb.net/shop?retryWrites=true&w=majority')
   .then(result => {
-    console.log('Connected Mongoose!')
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Khoa',
+          email: 'test@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    })
+
     app.listen(3000)
   })
   .catch(err => console.log(err))
