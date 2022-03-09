476fb894dfdf74bec72fb4c46557477636f1eca2
KhoaLe <leykhoaqk@gmail.com>
2022-03-09 10:30:04 +0700

    LAB 8.1

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,6 +8,7 @@ exports.getAddProduct = (req, res, next) => {
 		pageTitle: 'Add Product',
 		path: '/admin/add-product',
 		editing: false,
+		isAuthenticated: req.isLoggedIn
 	});
 };
 
@@ -46,6 +47,7 @@ exports.getEditProduct = (req, res, next) => {
 				path: '/admin/edit-product',
 				editing: editMode,
 				product: product,
+				isAuthenticated: req.isLoggedIn
 			});
 		})
 		.catch((err) => {
@@ -98,6 +100,7 @@ exports.getProducts = (req, res, next) => {
 				prods: products,
 				pageTitle: 'Admin Products',
 				path: '/admin/products',
+				isAuthenticated: req.isLoggedIn
 			});
 		})
 		.catch((err) => {
