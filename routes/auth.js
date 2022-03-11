d6d33b76bfbd870a921985a3f22ef58c0dda304c
KhoaLe <leykhoaqk@gmail.com>
2022-03-12 00:45:45 +0700

    LAB 9.4

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -12,15 +12,25 @@ router.post('/login', authController.postLogin);
 router.get('/signup', authController.getSignup);
 router.post(
   '/signup',
-  check('email')
-    .isEmail()
-    .withMessage('Please enter a valid email')
-    .custom((value, { req }) => {
-      if (value === 'khoale@gg') {
-        throw new Error('This email address if forbidden');
-      }
-      return true;
-    }),
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom((value, { req }) => {
+        if (value === 'khoale@gg') {
+          throw new Error(
+            'This email address if forbidden'
+          );
+        }
+        return true;
+      }),
+    body(
+      'password',
+      'Please enter a password with only numbers and text and at least 5 characters!'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+  ],
   authController.postSignup
 );
 
